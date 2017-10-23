770f36b0537c01e470cd97c7e5443c2d0472b9a5
Martin Abadie <martin.abadie@gmail.com>
2017-10-23 11:42:13 +0200

    Fix issues

--- index.js
+++ index.js
@@ -29,13 +29,6 @@ io.sockets.on('connection', function(socket) {
     });
 });
 
-io.sockets.on('message', function(message) {
-    console.log('New message');
-    ds18b20.temperature('28-051684eebbff', function(err, value) {
-        socket.emit('message', value);
-        console.log('send value');
-    });
-});
 /*
 io.sockets.on('connection', function(socket) {
     console.log('New connection');
